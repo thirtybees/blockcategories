7ac606c83ac01773ebdbab5d13aa974a2764f3f4
Markus Hitter <mah@jump-ing.de>
2019-06-27 21:57:28 +0200

    Make release 3.0.2.

--- blockcategories.php
+++ blockcategories.php
@@ -43,7 +43,7 @@ class BlockCategories extends Module
     {
         $this->name = 'blockcategories';
         $this->tab = 'front_office_features';
-        $this->version = '3.0.1';
+        $this->version = '3.0.2';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
 
